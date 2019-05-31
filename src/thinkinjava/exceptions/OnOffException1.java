df4d4776dec81a8bce94cd5d4dc971a227e9fccd
yinxing <yinxing@dxy.cn>
2019-05-31 10:22:24 +0800

    [add]新增think in java 代码

---
+++ src/thinkinjava/exceptions/OnOffException1.java
@@ -0,0 +1,2 @@
+package thinkinjava.exceptions;//: exceptions/OnOffException1.java
+public class OnOffException1 extends Exception {} ///:~
---
+++ src/thinkinjava/exceptions/OnOffException2.java
@@ -0,0 +1,2 @@
+package thinkinjava.exceptions;//: exceptions/OnOffException2.java
+public class OnOffException2 extends Exception {} ///:~
