44fabaddbb819fc946b88a3f3448cabce0230a2b
yinxing <yinxing@dxy.cn>
2019-08-16 20:11:59 +0800

    [add]16 在公共类中使用访问方法而不是公共属性

---
+++ src/effectivejava/item16/Point.java
@@ -0,0 +1,42 @@
+package effectivejava.item16;
+
+/**
+ * 正例:通过set/get方法来封装数据
+ * <p>
+ * 【结论】公共类不应该暴露可变属性。 公共累暴露不可变属性的危害虽然仍然存在问题，但其危害较小。
+ *
+ * @author yinxing
+ * @date 2019/8/16
+ */
+
+/**
+ * 通过set/get方法来封装数据
+ * Encapsulation of data by accessor methods and mutators
+ */
+public class Point {
+
+    private double x;
+
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ src/effectivejava/item16/Point1.java
@@ -0,0 +1,18 @@
+package effectivejava.item16;
+
+/**
+ * @author yinxing
+ * @date 2019/8/16
+ */
+
+/**
+ * 坏代码的味道:
+ * 退化类: 除了集中实例域，没有说明作用，属性不应该是 public
+ */
+class Point1 {
+
+    public double x;
+
+    public double y;
+
+}
