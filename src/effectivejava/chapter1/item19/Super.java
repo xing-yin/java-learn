74095b3071250309795ab5a4ce199c2205e6cc44
yinxing <yinxing@dxy.cn>
2019-08-26 10:19:08 +0800

    [add]19 要么继承提供文档，要么禁止继承

---
+++ src/effectivejava/chapter1/item19/Super.java
@@ -0,0 +1,22 @@
+package effectivejava.chapter1.item19;
+
+/**
+ * 反例: 构造方法绝不能直接或间接调用可重写的方法。如果违反这个规则，将导致程序失败。
+ * @author yinxing
+ * @date 2019/8/23
+ */
+
+public class Super {
+
+    /**
+     * Broken - constructor invokes an overridable method
+     * 构造方法引用了一个重写的方法
+     */
+    public Super() {
+        overrideMethod();
+    }
+
+    public void overrideMethod() {
+    }
+
+}
