f3d5c975c666aa327dd9686ae4a5d1a0b901b22c
yinxing <yinxing@dxy.cn>
2019-09-04 19:45:25 +0800

    [add]33 优先考虑类型安全的异构容器

---
+++ src/effectivejava/chapter4/item34/AppleEnum.java
@@ -0,0 +1,16 @@
+package effectivejava.chapter4.item34;
+
+/**
+ * 正例:Java 提供了一种避免 int 和 String 枚举模式的所有缺点的替代方法，并提供了许多额外的好处。它是枚举类型。
+ *
+ * @author yinxing
+ * @date 2019/9/4
+ */
+
+public enum AppleEnum {
+
+    /**
+     * category
+     */
+    FUJI, PIPPIN, GRANNY_SMITH
+}
